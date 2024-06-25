fc898114a1a8ed704c2ba385ca0e950c142d0a9e
FatmaDerya <133167233+FatmaDerya@users.noreply.github.com>
2024-06-25 13:13:27 +0200

    Animal, User en Adoption klassen aangemaakt. Attributen, constructors en methoden toegevoegd.

--- src/main/java/model/AdoptionRequest.java
+++ src/main/java/model/AdoptionRequest.java
@@ -1,2 +1,78 @@
-package model;public class AdoptionRequest {
+package model;
+
+import java.util.Date;
+
+public class AdoptionRequest {
+
+    // Attributes
+    private  int idAdoption;
+    private  int idUser;
+    private int idAnimal;
+    private Date requestDate;
+    private String status;
+
+
+    // constructors
+    public AdoptionRequest(int idAdoption, int idUser, int idAnimal, Date requestDate, String status){
+        this.idAdoption = idAdoption;
+        this.idUser = idUser;
+        this.idAnimal = idAnimal;
+        this.requestDate =requestDate;
+        this.status = status;
+
+
+    }
+    
+// Getters & Setters
+
+    public int getIdAdoption() {
+        return idAdoption;
+    }
+
+    public void setIdAdoption(int idAdoption) {
+        this.idAdoption = idAdoption;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public int getIdAnimal() {
+        return idAnimal;
+    }
+
+    public void setIdAnimal(int idAnimal) {
+        this.idAnimal = idAnimal;
+    }
+
+    public Date getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(Date requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+// Methode
+    @Override
+    public String toString() {
+        return "AdoptionRequest{" +
+                "idAdoption=" + idAdoption +
+                ", idUser=" + idUser +
+                ", idAnimal=" + idAnimal +
+                ", requestDate=" + requestDate +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,2 +1,56 @@
-package model;public class User {
+package model;
+
+public class User {
+
+    // Attributes
+    private int idUser;
+    private String email;
+    private String password;
+    private String role;
+
+
+    // Constructors
+    public User (int idUser, String email, String password,String role){
+        this.idUser = idUser;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+
+    }
+    public User (String email, String password, String role){
+        this(0,email,password,role);
+    }
+     // Getters & Setters
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
