07b4fe57068e7b4ac922b7d9ede3c2c85ddec90b
FatmaDerya <133167233+FatmaDerya@users.noreply.github.com>
2024-07-03 13:25:08 +0200

    AnimalService aangemaakt en gekoppeld aan AnimalRepository

---
+++ src/main/java/animal/shelter/service/AnimalService.java
@@ -0,0 +1,41 @@
+package animal.shelter.service;
+
+import animal.shelter.model.Animal;
+import animal.shelter.repository.AnimalRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AnimalService {
+    AnimalRepository animalRepository;
+
+    public AnimalService(AnimalRepository animalRepository) {
+        this.animalRepository = animalRepository;
+
+    }
+
+
+    public void saveAnimal(Animal animal) {
+        animalRepository.saveAnimal(animal);
+
+    }
+
+    public Optional<Animal> findAnimalById(int idAnimal) {
+        return animalRepository.findAnimalById(idAnimal);
+    }
+
+    public List<Animal> findAllAnimal() {
+        return animalRepository.findAllAnimal();
+    }
+
+    public void updateAnimal(Animal animal) {
+        animalRepository.updateAnimal(animal);
+    }
+
+    public void deleteAnimal(Animal animal) {
+        animalRepository.deleteAnimal(animal);
+    }
+
+}
